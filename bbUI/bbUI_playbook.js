297b70da07aa4dd9c4f40fc694dbbf91909aa432
Sully Syed <sully@yllus.com>
2011-12-04 10:58:20 -0500

    Add usage of graphical icons.

--- bbUI/bbUI_playbook.js
+++ bbUI/bbUI_playbook.js
@@ -24,7 +24,14 @@ bb.tweetList = {
 						innerChildNode.setAttribute('onmouseout', "this.setAttribute('class','bb-hires-image-list-item')");
 						innerChildNode.setAttribute('x-blackberry-focusable','true');
 						innerChildNode.innerHTML = '<img src="'+ innerChildNode.getAttribute('data-bb-img') +'" />\n'+
-										'<div class="details" style="float: left; width: 855px; height: 73px;">\n'+
+										'<div class="details" style="width: 1019px; height: 73px;" onclick="alert(\'hi\');">\n'+
+										'	   <span class="title">' + innerChildNode.getAttribute('data-bb-screenname') + '</span>\n'+
+										'	   <div class="date">'+ innerChildNode.getAttribute('data-bb-date') +'</div>\n'+
+										'	   <div class="description">' + description + '</div>\n'+
+										'</div>\n';
+						/*
+						innerChildNode.innerHTML = '<img src="'+ innerChildNode.getAttribute('data-bb-img') +'" />\n'+
+										'<div class="details" style="float: left; width: 855px; height: 73px;" onclick="alert(\'hi\');">\n'+
 										'	   <span class="title">' + innerChildNode.getAttribute('data-bb-screenname') + '</span>\n'+
 										'	   <div class="date">'+ innerChildNode.getAttribute('data-bb-date') +'</div>\n'+
 										'	   <div class="description">' + description + '</div>\n'+
@@ -32,6 +39,7 @@ bb.tweetList = {
 										'<div style="float: left; width: 70px; height: 73px;">\n'+
 										'	   <div style="width: 50%; margin: auto; padding-top: 22px;"><img src="images/actions.png" style="width: 30px; height: 30px;" /></div>\n'+
 										'</div>\n';
+						 */
 					} else {
 						innerChildNode.setAttribute('class', 'bb-lowres-image-list-item');
 						innerChildNode.setAttribute('onmouseover', "this.setAttribute('class','bb-lowres-image-list-item-hover')");
