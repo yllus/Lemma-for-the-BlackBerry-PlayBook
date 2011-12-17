15c8b342f9446b6a3914761c93c49a7b20d0bb8b
Sully Syed <sully@yllus.com>
2011-12-17 13:42:49 -0500

    Re-add the postTweet function.

--- js/screens/compose.js
+++ js/screens/compose.js
@@ -1,3 +1,39 @@
+// Post a new status message to the user's own timeline.
+function postTweet() {
+	var url = 'https://api.twitter.com/1/statuses/update.json';
+	var status = document.getElementById('tweet_textarea').value;
+	var in_reply_to_status_id = document.getElementById('in_reply_to_status_id').innerHTML;
+	
+	// If the status is empty or more than 140 characters, just exit without posting to the Twitter API.
+	if ( status.length < 1 || status.length > 140 ) {
+		return;
+	}
+	
+	// If the tweet is in reply to an existing tweet, send in_reply_to_status_id along. Else omit that parameter.
+	var tweet_options = { 'status': status, 'trim_user': 'true', 'include_entities': 'false' }; 
+	if ( in_reply_to_status_id.length > 0 ) {
+		tweet_options = { 'status': status, 'in_reply_to_status_id': in_reply_to_status_id, 'trim_user': 'true', 'include_entities': 'false' };
+	}
+	
+	oauth.post(url, 
+		tweet_options,
+		
+		// Show the success message. 
+		function(data) {
+			//var json_data = JSON.parse(data.text);
+			
+			document.getElementById('tweet_result').innerHTML = '<span style="color: #009933;">Success!</span>';
+			document.getElementById('tweet_textarea').value = '';
+		},
+		 
+		// Show a failure message.
+		function(data) { 
+			document.getElementById('tweet_result').innerHTML = '<span style="color: #FF0000;">Error, please retry.</span>';
+			//document.getElementById('tweet_textarea').value = '';
+		}
+	);
+}
+
 function focus_compose( element ) {
 	setTimeout("do_focus();", 750);
 }
